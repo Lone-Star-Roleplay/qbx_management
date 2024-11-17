998856ac481dcc4558e91eb93b2b61b2ccfc974b
James <imblazeitjames@gmail.com>
2024-11-17 17:35:11 -0600

    fix(config): update minimum on-duty log time and modify zone definitions

--- config/server.lua
+++ config/server.lua
@@ -1,23 +1,36 @@
 return {
     discordWebhook = nil, -- Replace nil with your webhook if you chose to use discord logging over ox_lib logging
-    minOnDutyLogTimeMinutes = 30,
+    minOnDutyLogTimeMinutes = 15,
     formatDateTime = '%m-%d-%Y %H:%M',
 
     -- While the config boss menu creation still works, it is recommended to use the runtime export instead.
     ---@alias GroupName string
+
+    ---@class ZoneInfo
+    ---@field coords vec3 coordinates of the zone
+    ---@field size vec3 size of the zone
+    ---@field rotation number rotation of the zone
+    ---@field type GroupType
+
     ---@type table<GroupName, ZoneInfo>
     menus = {
-        lostmc = {
-            coords = vec3(983.69, -90.92, 74.85),
+        mosleys = {
+            coords = vec3(-15.53, -1659.19, 33.04),
             size = vec3(1.5, 1.5, 1.5),
             rotation = 39.68,
-            type = 'gang',
+            type = 'job',
         },
-        vagos = {
-            coords = vec3(351.18, -2054.92, 22.09),
+        reporter = {
+            coords = vec3(-80.69, -802.2, 243.4),
             size = vec3(1.5, 1.5, 1.5),
             rotation = 39.68,
-            type = 'gang',
+            type = 'job',
+        },
+        police = {
+            coords = vec3(462.1, -985.55, 30.73),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 0,
+            type = 'job',
         },
     },
 }
